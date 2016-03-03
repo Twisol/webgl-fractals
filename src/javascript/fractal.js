3a95d087268ed3637991b026c36a429dcc808955
Jonathan Castello <twisolar@gmail.com>
2016-03-03 02:24:45 -0800

    Refactor towards ECMAScript 6, and add a new default set of parameters ('magma chamber')

--- src/javascript/fractal.js
+++ src/javascript/fractal.js
@@ -1,4 +1,4 @@
-var Models = {
+let Models = {
   "quad": new Float32Array([
     -1, -1,
     -1,  1,
@@ -31,8 +31,8 @@ function polyroots(roots) {
 
 // Takes the derivative of a complex polynomial
 function polyD(poly) {
-  var D = [];
-  for (var i = 1; i < poly.length; ++i) {
+  let D = [];
+  for (let i = 1; i < poly.length; ++i) {
     D.push(compmul([i, 0], poly[i]));
   }
   D.push([0, 0]);
@@ -44,36 +44,32 @@ function xhrContent(xhr) {
   return xhr.response;
 }
 
-function makeShaderProgram(gl, roots, exponent, iterations) {
-  return $.fetch("/shaders/frac_fragment.glsl").then(xhrContent).then(function(fragmentShaderSource) {
-    return $.fetch("/shaders/frac_vertex.glsl").then(xhrContent).then(function(vertexShaderSource) {
-      fragmentShaderSource = fragmentShaderSource.replace(/\{\{EXPONENT\}\}/g, ""+exponent).replace(/\{\{ITERATIONS\}\}/g, ""+iterations).replace(/\{\{NUMROOTS\}\}/g, ""+roots.length);
+function makeShaderProgram(gl, fragmentShaderSource, vertexShaderSource, roots, exponent, iterations) {
+  fragmentShaderSource = fragmentShaderSource.replace(/\{\{ITERATIONS\}\}/g, ""+iterations).replace(/\{\{NUMROOTS\}\}/g, ""+roots.length);
 
-      var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-      gl.shaderSource(fragmentShader, fragmentShaderSource);
-      gl.compileShader(fragmentShader);
-      if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-        console.log(gl.getShaderInfoLog(fragmentShader));
-      }
+  let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+  gl.shaderSource(fragmentShader, fragmentShaderSource);
+  gl.compileShader(fragmentShader);
+  if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+    console.log(gl.getShaderInfoLog(fragmentShader));
+  }
 
-      var vertexShader = gl.createShader(gl.VERTEX_SHADER);
-      gl.shaderSource(vertexShader, vertexShaderSource);
-      gl.compileShader(vertexShader);
-      if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
-        console.log(gl.getShaderInfoLog(vertexShader));
-      }
+  let vertexShader = gl.createShader(gl.VERTEX_SHADER);
+  gl.shaderSource(vertexShader, vertexShaderSource);
+  gl.compileShader(vertexShader);
+  if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+    console.log(gl.getShaderInfoLog(vertexShader));
+  }
 
-      var program = gl.createProgram();
-      gl.attachShader(program, fragmentShader);
-      gl.attachShader(program, vertexShader);
-      gl.linkProgram(program);
-      if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-        console.log(gl.getProgramInfoLog(program));
-      }
+  let program = gl.createProgram();
+  gl.attachShader(program, fragmentShader);
+  gl.attachShader(program, vertexShader);
+  gl.linkProgram(program);
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    console.log(gl.getProgramInfoLog(program));
+  }
 
-      return program;
-    });
-  });
+  return program;
 }
 
 
@@ -95,27 +91,28 @@ function Fractal(gl, model, settings, shaderProgram) {
 }
 
 Fractal.prototype.draw = function() {
-  var gl = this.gl;
+  let gl = this.gl;
+  let shaderProgram = this.shaderProgram;
 
   // Render the tile geometry
-  gl.useProgram(this.shaderProgram);
+  gl.useProgram(shaderProgram);
 
-  var vertexLocation = gl.getAttribLocation(this.shaderProgram, "a_vertex");
-  var aspectLocation = gl.getUniformLocation(this.shaderProgram, "u_aspect");
+  let vertexLocation = gl.getAttribLocation(shaderProgram, "a_vertex");
+  let aspectLocation = gl.getUniformLocation(shaderProgram, "u_aspect");
 
-  var polyLocation = gl.getUniformLocation(this.shaderProgram, "u_poly");
-  var derivLocation = gl.getUniformLocation(this.shaderProgram, "u_deriv");
-  var rootsLocation = gl.getUniformLocation(this.shaderProgram, "u_roots");
-  var centerLocation = gl.getUniformLocation(this.shaderProgram, "u_center");
-  var zoomLocation = gl.getUniformLocation(this.shaderProgram, "u_zoom");
-  var brightnessLocation = gl.getUniformLocation(this.shaderProgram, "u_brightness");
-  var colorLocation = gl.getUniformLocation(this.shaderProgram, "u_color");
-  var rootRadiusLocation = gl.getUniformLocation(this.shaderProgram, "u_root_radius");
-  var aLocation = gl.getUniformLocation(this.shaderProgram, "u_a");
-  var epsLocation = gl.getUniformLocation(this.shaderProgram, "u_eps");
+  let polyLocation = gl.getUniformLocation(shaderProgram, "u_poly");
+  let derivLocation = gl.getUniformLocation(shaderProgram, "u_deriv");
+  let rootsLocation = gl.getUniformLocation(shaderProgram, "u_roots");
+  let centerLocation = gl.getUniformLocation(shaderProgram, "u_center");
+  let zoomLocation = gl.getUniformLocation(shaderProgram, "u_zoom");
+  let brightnessLocation = gl.getUniformLocation(shaderProgram, "u_brightness");
+  let colorLocation = gl.getUniformLocation(shaderProgram, "u_color");
+  let rootRadiusLocation = gl.getUniformLocation(shaderProgram, "u_root_radius");
+  let aLocation = gl.getUniformLocation(shaderProgram, "u_a");
+  let epsLocation = gl.getUniformLocation(shaderProgram, "u_eps");
 
-  var poly = polyroots(this.settings.roots);
-  var deriv = polyD(poly);
+  let poly = polyroots(this.settings.roots);
+  let deriv = polyD(poly);
 
   gl.uniform2fv(rootsLocation, Array.prototype.concat.apply([], this.settings.roots));
   gl.uniform2fv(polyLocation, Array.prototype.concat.apply([], poly));
@@ -140,7 +137,6 @@ Fractal.prototype.draw = function() {
 
 Fractal.prototype.set = function(prop, value) {
   this.settings[prop] = value;
-  this.redraw = true;
 };
 
 window.saveImage = function(filename) {
@@ -169,29 +165,33 @@ window.onkeyup = function(ev) {
 }
 
 $.fetch("/parameters.json", {responseType: "json"}).then(xhrContent).then(function(settings) {
-  // Create a canvas
-  var canvas = document.getElementById("canvas");
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+  $.fetch("/shaders/frac_fragment.glsl").then(xhrContent).then(function(fragmentShaderSource) {
+    $.fetch("/shaders/frac_vertex.glsl").then(xhrContent).then(function(vertexShaderSource) {
+      // Create a canvas
+      let canvas = document.getElementById("canvas");
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
 
-  // Get the WebGL context
-  var gl = canvas.getContext("webgl", {
-    antialias: true,
-    preserveDrawingBuffer: true  // enable saving the canvas as an image.
-  });
+      // Get the WebGL context
+      let gl = canvas.getContext("webgl", {
+        antialias: true,
+        preserveDrawingBuffer: true  // enable saving the canvas as an image.
+      });
 
-  // Load the surface geometry into GPU memory
-  var buffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-  gl.bufferData(gl.ARRAY_BUFFER, Models["quad"], gl.STATIC_DRAW);
+      // Load the surface geometry into GPU memory
+      let buffer = gl.createBuffer();
+      gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+      gl.bufferData(gl.ARRAY_BUFFER, Models["quad"], gl.STATIC_DRAW);
 
-  makeShaderProgram(gl, settings.roots, settings.exponent, settings.iterations).then(function(program) {
-    // Set up the fractal generator
-    var world = new Fractal(gl, buffer, settings, program);
-    window.world = world;
+      let program = makeShaderProgram(gl, fragmentShaderSource, vertexShaderSource, settings.roots, settings.exponent, settings.iterations);
 
-    requestAnimationFrame(function onAnimationFrame() {
-      world.draw();
+      // Set up the fractal generator
+      let world = new Fractal(gl, buffer, settings, program);
+      window.world = world;
+
+      requestAnimationFrame(function onAnimationFrame() {
+        world.draw();
+      });
     });
-  })
+  });
 });
