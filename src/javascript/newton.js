404581188f8312b4be8cc32a18377bd11a0ba745
Jonathan Castello <twisolar@gmail.com>
2016-03-17 02:18:19 -0700

    Refactor newton.js to modularize the complex and polynomial algebraic operations

--- src/javascript/newton.js
+++ src/javascript/newton.js
@@ -1,61 +1,64 @@
-function complex_add(a, b) {
-  return [a[0] + b[0], a[1] + b[1]];
-}
+export const Complex = {
+  add(a, b) {
+    return [a[0] + b[0], a[1] + b[1]];
+  },
 
-function complex_multiply(a, b) {
-  return [a[0]*b[0] - a[1]*b[1], a[0]*b[1] + a[1]*b[0]];
-}
+  multiply(a, b) {
+    return [a[0]*b[0] - a[1]*b[1], a[0]*b[1] + a[1]*b[0]];
+  },
+};
 
+export const Poly = {
+  add(p1, p2) {
+    if (p1.length < p2.length) {
+      let tmp = p1;
+      p1 = p2;
+      p2 = tmp;
+    }
+    // p1 is now at least as long as p2
 
-function poly_add(p1, p2) {
-  if (p1.length < p2.length) {
-    let tmp = p1;
-    p1 = p2;
-    p2 = tmp;
-  }
-  // p1 is now at least as long as p2
+    // Get the higher-order terms from p1 which aren't affected
+    let p = p1.slice(0, p1.length - p2.length);
+    p1 = p1.slice(p1.length - p2.length);
 
-  // Get the higher-order terms from p1 which aren't affected
-  let p = p1.slice(0, p1.length - p2.length);
-  p1 = p1.slice(p1.length - p2.length);
-
-  // Sum the lower-order terms
-  for (let i = 0; i < p1.length; i += 1) {
-    p.push(complex_add(p1[i], p2[i]));
-  }
-
-  return p;
-}
-
-function poly_multiply(p1, p2) {
-  let p = [];
-
-  for (let i = 0; i <= Math.max(p1.length, p2.length); i += 1) {
-  // For every term in the result polynomial
-    let term = [0, 0];
-
-    for (let a = 0; a < p1.length; a += 1) {
-      let b = i - a;
-      if (b < 0) {
-        break;
-      } else if (b >= p2.length) {
-        continue;
-      }
-
-      term = complex_add(term, complex_multiply(p1[a], p2[b]));
+    // Sum the lower-order terms
+    for (let i = 0; i < p1.length; i += 1) {
+      p.push(Complex.add(p1[i], p2[i]));
     }
 
-    p.push(term);
-  }
+    return p;
+  },
 
-  return p;
-}
+  multiply(p1, p2) {
+    let p = [];
+
+    for (let i = 0; i <= Math.max(p1.length, p2.length); i += 1) {
+    // For every term in the result polynomial
+      let term = [0, 0];
+
+      for (let a = 0; a < p1.length; a += 1) {
+        let b = i - a;
+        if (b < 0) {
+          break;
+        } else if (b >= p2.length) {
+          continue;
+        }
+
+        term = Complex.add(term, Complex.multiply(p1[a], p2[b]));
+      }
+
+      p.push(term);
+    }
+
+    return p;
+  },
+};
 
 
 function getNumerator(roots) {
   let poly = [[1, 0]];
   for (let i = 0; i < roots.length; i += 1) {
-    poly = poly_multiply(poly, [[1, 0], complex_multiply([-1, 0], roots[i])]);
+    poly = Poly.multiply(poly, [[1, 0], Complex.multiply([-1, 0], roots[i])]);
   }
   return poly;
 }
@@ -66,10 +69,10 @@ function getDenominator(roots, multiplicities) {
     let term = [multiplicities[i]];
     for (let j = 0; j < roots.length; j += 1) {
       if (j !== i) {
-        term = poly_multiply(term, [[1, 0], complex_multiply([-1, 0], roots[j])]);
+        term = Poly.multiply(term, [[1, 0], Complex.multiply([-1, 0], roots[j])]);
       }
     }
-    poly = poly_add(poly, term);
+    poly = Poly.add(poly, term);
   }
   return poly;
 }
@@ -77,7 +80,7 @@ function getDenominator(roots, multiplicities) {
 export function preprocessPolynomial(roots, multiplicities) {
   let numerator = [[1, 0]];
   for (let i = 0; i < roots.length; i += 1) {
-    numerator = poly_multiply(numerator, [[1, 0], complex_multiply([-1, 0], roots[i])]);
+    numerator = Poly.multiply(numerator, [[1, 0], Complex.multiply([-1, 0], roots[i])]);
   }
 
   let denominator = [[0, 0]];
@@ -85,10 +88,10 @@ export function preprocessPolynomial(roots, multiplicities) {
     let term = [multiplicities[i]];
     for (let j = 0; j < roots.length; j += 1) {
       if (j !== i) {
-        term = poly_multiply(term, [[1, 0], complex_multiply([-1, 0], roots[j])]);
+        term = Poly.multiply(term, [[1, 0], Complex.multiply([-1, 0], roots[j])]);
       }
     }
-    denominator = poly_add(denominator, term);
+    denominator = Poly.add(denominator, term);
   }
 
   return {numerator, denominator};
