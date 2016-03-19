4a3680036048ad813b97431e89262afcaf6796d6
Jonathan Castello <twisolar@gmail.com>
2016-03-19 01:12:27 -0700

    Continue refactoring of Fractal class

--- src/javascript/fractal.js
+++ src/javascript/fractal.js
@@ -5,57 +5,66 @@ import Models from "./models";
 
 function Fractal(settings) {
   this.settings = settings;
-
   this.forcedRedraw = false;
-  this.keys = {
-    up: 0,
-    down: 0,
-    left: 0,
-    right: 0,
-    zoomin: 0,
-    zoomout: 0
-  };
+  this.numerator = null;
+  this.denominator = null;
 }
 
-Fractal.prototype.update = function() {
-  let shouldRedraw = false;
+function update_world(world, keys) {
+  let settings = world.settings;
+  let shouldRedraw = world.forcedRedraw;
 
-  if (this.forcedRedraw) {
-    this.forcedRedraw = false;
+  let center = [settings.center[0], settings.center[1]];
+  let zoom = settings.zoom;
+
+  if (keys.up !== keys.down) {
+    center[1] += (keys.up - keys.down)/(30*settings.zoom);
     shouldRedraw = true;
   }
-  if (this.keys.up - this.keys.down != 0) {
-    this.settings.center[1] += (this.keys.up - this.keys.down)/(30*this.settings.zoom);
+  if (keys.right !== keys.left) {
+    center[0] += (keys.right - keys.left)/(30*settings.zoom);
     shouldRedraw = true;
   }
-  if (this.keys.right - this.keys.left != 0) {
-    this.settings.center[0] += (this.keys.right - this.keys.left)/(30*this.settings.zoom);
-    shouldRedraw = true;
-  }
-  if (this.keys.zoomin - this.keys.zoomout != 0) {
-    this.settings.zoom *= Math.pow(0.990, this.keys.zoomin - this.keys.zoomout);
+  if (keys.zoomin !== keys.zoomout) {
+    zoom *= Math.pow(0.990, keys.zoomin - keys.zoomout);
     shouldRedraw = true;
   }
 
-  return shouldRedraw;
-};
+  let new_world = new Fractal({
+    center: center,
+    zoom: zoom,
+    brightness: settings.brightness,
+    root_radius: settings.root_radius,
 
-Fractal.prototype.load_uniforms = function(gl, shader) {
+    roots: settings.roots,
+    multiplicities: settings.multiplicities,
+    colors: settings.colors,
+
+    iterations: settings.iterations,
+    eps: settings.eps,
+  });
+  new_world.forcedRedraw = shouldRedraw;
+  new_world.numerator = world.numerator;
+  new_world.denominator = world.denominator;
+  return new_world;
+}
+
+function upload_uniforms(gl, shader, world) {
   gl.useProgram(shader.program);
 
-  gl.uniform2fv(shader.locations["u_roots"], Array.prototype.concat.apply([], this.settings.roots));
-  gl.uniform2fv(shader.locations["u_numerator"], Array.prototype.concat.apply([], this.settings.numerator));
-  gl.uniform2fv(shader.locations["u_denominator"], Array.prototype.concat.apply([], this.settings.denominator));
-  gl.uniform3fv(shader.locations["u_colors"], Array.prototype.concat.apply([], this.settings.colors));
-  gl.uniform2fv(shader.locations["u_center"], this.settings.center);
-  gl.uniform1f(shader.locations["u_zoom"], this.settings.zoom);
-  gl.uniform1f(shader.locations["u_brightness"], this.settings.brightness);
-  gl.uniform1f(shader.locations["u_root_radius"], this.settings.root_radius);
-  gl.uniform1f(shader.locations["u_eps"], this.settings.eps);
+  gl.uniform2fv(shader.locations["u_roots"], Array.prototype.concat.apply([], world.settings.roots));
+  gl.uniform2fv(shader.locations["u_numerator"], Array.prototype.concat.apply([], world.numerator));
+  gl.uniform2fv(shader.locations["u_denominator"], Array.prototype.concat.apply([], world.denominator));
+  gl.uniform3fv(shader.locations["u_colors"], Array.prototype.concat.apply([], world.settings.colors));
+  gl.uniform2fv(shader.locations["u_center"], world.settings.center);
+  gl.uniform1f(shader.locations["u_zoom"], world.settings.zoom);
+  gl.uniform1f(shader.locations["u_brightness"], world.settings.brightness);
+  gl.uniform1f(shader.locations["u_root_radius"], world.settings.root_radius);
+  gl.uniform1f(shader.locations["u_eps"], world.settings.eps);
   gl.uniform1f(shader.locations["u_aspect"], canvas.width/canvas.height);
 };
 
-Fractal.prototype.draw = function(gl, shader, mesh) {
+function draw_fractal(gl, shader, mesh) {
 /// Set up GL state
   // Load shader state
   gl.useProgram(shader.program);
@@ -110,6 +119,20 @@ $.fetch("parameters.json").then(function(xhr) {
   let settings = JSON.parse(xhr.response);
   $("#parameters").value = xhr.response;
 
+  // Set up the fractal generator
+  let world = new Fractal(settings);
+  window.world = world;
+
+  // Keep track of user input
+  let keys = {
+    up: 0,
+    down: 0,
+    left: 0,
+    right: 0,
+    zoomin: 0,
+    zoomout: 0,
+  };
+
   Shader.fetch(FRACTAL_SHADER_SCHEMA).then(function(base_shader_schema) {
     // Create a canvas
     let canvas = document.getElementById("canvas");
@@ -134,22 +157,19 @@ $.fetch("parameters.json").then(function(xhr) {
     }));
 
     ({
-      numerator: settings.numerator,
-      denominator: settings.denominator,
+      numerator: world.numerator,
+      denominator: world.denominator,
     } = preprocessPolynomial(settings.roots, settings.multiplicities));
-
-    // Set up the fractal generator
-    let world = new Fractal(settings);
-    window.world = world;
 
     world.forcedRedraw = true;
     requestAnimationFrame(function onAnimationFrame() {
       requestAnimationFrame(onAnimationFrame);
 
-      let shouldRedraw = world.update();
-      if (shouldRedraw) {
-        world.load_uniforms(gl, shader);
-        world.draw(gl, shader, mesh);
+      world = update_world(world, keys);
+
+      if (world.forcedRedraw) {
+        upload_uniforms(gl, shader, world);
+        draw_fractal(gl, shader, mesh);
       }
     });
 
@@ -175,12 +195,9 @@ $.fetch("parameters.json").then(function(xhr) {
       ||  JSON.stringify(settings.multiplicities) !== JSON.stringify(world.settings.multiplicities)) {
         console.log("Recreating polynomials");
         ({
-          numerator: settings.numerator,
-          denominator: settings.denominator,
+          numerator: world.numerator,
+          denominator: world.denominator,
         } = preprocessPolynomial(settings.roots, settings.multiplicities));
-      } else {
-        settings.numerator = world.settings.numerator;
-        settings.denominator = world.settings.denominator;
       }
 
       world.forcedRedraw = true;
@@ -189,22 +206,22 @@ $.fetch("parameters.json").then(function(xhr) {
 
     canvas.addEventListener("keydown", function(ev) {
       switch (ev.keyCode) {
-      case 65: world.keys.left = 1; break;
-      case 68: world.keys.right = 1; break;
-      case 87: world.keys.up = 1; break;
-      case 83: world.keys.down = 1; break;
-      case 16: world.keys.zoomin = 1; break;
-      case 32: world.keys.zoomout = 1; break;
+      case 65: keys.left = 1; break;
+      case 68: keys.right = 1; break;
+      case 87: keys.up = 1; break;
+      case 83: keys.down = 1; break;
+      case 16: keys.zoomin = 1; break;
+      case 32: keys.zoomout = 1; break;
       }
     });
     canvas.addEventListener("keyup", function(ev) {
       switch (ev.keyCode) {
-      case 65: world.keys.left = 0; break;
-      case 68: world.keys.right = 0; break;
-      case 87: world.keys.up = 0; break;
-      case 83: world.keys.down = 0; break;
-      case 16: world.keys.zoomin = 0; break;
-      case 32: world.keys.zoomout = 0; break;
+      case 65: keys.left = 0; break;
+      case 68: keys.right = 0; break;
+      case 87: keys.up = 0; break;
+      case 83: keys.down = 0; break;
+      case 16: keys.zoomin = 0; break;
+      case 32: keys.zoomout = 0; break;
       }
     });
     canvas.addEventListener("click", function(ev) {
